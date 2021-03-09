ad4a322e8a7c6ac2af20e78a59ab8bb476f1aca1
Tamaza <igrigolia@wpi.edu>
2021-03-09 18:04:07 -0500

    ,,,

--- Bomberman/groupNN/scenario2/variant2.py
+++ Bomberman/groupNN/scenario2/variant2.py
@@ -9,7 +9,7 @@ from game import Game
 from monsters.stupid_monster import StupidMonster
 
 # TODO This is your code!
-sys.path.insert(1, '../group13)
+sys.path.insert(1, '../group13')
 from expecti_max_AI import AI
 
 # Create the game
